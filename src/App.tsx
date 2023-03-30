ec3202cae614b4fe8cfe0fd54b7291a081133d93
Annop Kongdeedai <annopkongdeedai@gmail.com>
2023-03-30 08:46:04 +0700

    add: init font

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-x-hidden">
+      <div className="relative grid grid-cols-2 gap-x-0 pt-6">
+        {/* Line 1 */}
+        <div className="bg-white flex flex-row justify-center">
+          <img
+            src="/images/footballer_1.png"
+            width={678}
+            height={950}
+            className="absolute top-[35px]"
+          />
+        </div>
+        <div className="bg-white flex flex-col pb-[56px]">
+          <span className="text-[90px] leading-[1.17] text-textsecondary">
+            ATHLETS
+          </span>
+          <div className="flex flex-row items-center mt-[59px]">
+            <div className="flex flex-col">
+              <span className="text-lg">01</span>
+              <div className="bg-purple w-[19px] h-[5px] rounded-[2.5px] mt-1" />
+            </div>
+            <span className="text-4xl text-textthird ml-[10px]">
+              CONNECTION
+            </span>
+          </div>
+          <span className="text-xl mt-[31px]">
+            Connect with coaches directly, you can ping coaches to view profile.
+          </span>
+        </div>
+
+        {/* Line 2 */}
+        <div className="bg-[#F5F4F9]" />
+        <div className="bg-[#F5F4F9] flex flex-col pt-[53px] pb-[51px]">
+          <div className="flex flex-row items-center">
+            <div className="flex flex-col">
+              <span className="text-lg">02</span>
+              <div className="bg-purple w-[19px] h-[5px] rounded-[2.5px] mt-1" />
+            </div>
+            <span className="text-4xl text-textthird ml-[10px]">
+              COLLABORATION
+            </span>
+          </div>
+          <span className="text-xl mt-[21px]">
+            Work with other student athletes to  increase visability. When you
+            share and like other players videos it will increase your visability
+            as a player. This is the team work aspect to Surface 1.
+          </span>
+        </div>
+
+        {/* Line 3 */}
+        <div className="bg-[#5E3DB3]" />
+        <div className="bg-[#5E3DB3] flex flex-col pt-[85px] pb-[98px]">
+          <div className="flex flex-row items-center">
+            <div className="flex flex-col">
+              <span className="text-lg">02</span>
+              <div className="bg-white w-[19px] h-[5px] rounded-[2.5px] mt-1" />
+            </div>
+            <span className="text-4xl text-textthird ml-[10px]">GROWTH</span>
+          </div>
+          <span className="text-xl text-white mt-[21px]">
+            Resources and tools for you to get better as a student Athelte.
+            Access to training classes, tutor sessions, etc 
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      "roboto-regular": ["Roboto-Regular"],
+    },
+    fontSize: {
+      lg: ["18px", "21px"],
+      xl: ["20px", "28px"],
+      "4xl": ["36px", "42px"],
+    },
+    colors: {
+      textprimary: "#000000",
+      textsecondary: "#E7E7E7",
+      textthird: "#C2C2C2",
+      purple: "#603EBE",
+    },
     extend: {},
   },
   plugins: [],
